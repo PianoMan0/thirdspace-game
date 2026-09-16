bd07833d809f0d6cbc414298160ed0992b117614
jyoshitanair <jyoshitanair123@gmail.com>
2026-09-15 18:42:54 -0700

    so like. what am i doing T-T

---
+++ other.js
@@ -0,0 +1,7 @@
+//gonna put my stuff here cause yeah :P
+
+console.log("EHLLO>");
+document.getElementById("rat").addEventListener('click', function() {
+  console.log("click detected");
+  window.location.href = 'meow.html';
+});
